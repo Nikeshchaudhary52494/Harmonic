2c45ac488ba7d42a2aafe5b76a6a112a4722c966
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-04-16 13:33:22 +0530

    layout completed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -22,6 +22,14 @@ export default function Home() {
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest songs </h1>
+        </div>
+        <div>
+          <p className="text-white">List of songs</p>
+        </div>
+      </div>
 
     </div>
   );
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import { useRouter } from "next/navigation"
+import { FaPlay } from "react-icons/fa"
+import { MdFavorite } from "react-icons/md"
+
 type ListItemProps = {
     image: string,
     name: string,
@@ -11,10 +15,31 @@ const ListItem: React.FC<ListItemProps> = ({
     image,
     href
 }) => {
+
+    const router = useRouter();
+
+    const onClick = () => {
+        // add authentication before push
+        router.push(href);
+    }
+
     return (
-        <div>
-            List Items...
-        </div>
+        <button
+            onClick={onClick}
+            className="flex relative bg-neutral-100/10 group hover:bg-neutral-100/20 rounded-md transition items-center gap-x-4 ">
+            <div className="min-h-[64px] min-w-[64px]  ">
+                <MdFavorite
+                    className="text-white bg-gradient-to-br h-full w-full p-4 from-blue-700 to-blue-300 rounded-lg aspect-square"
+                />
+
+            </div>
+            <p className="text-neutral-400 group-hover:text-white transition font-medium">
+                Liked songs
+            </p>
+            <div className="rounded-full opacity-0 absolute group-hover:opacity-100 transition hover:scale-110 bg-green-500 p-4 right-5">
+                <FaPlay />
+            </div>
+        </button>
     )
 }
 
